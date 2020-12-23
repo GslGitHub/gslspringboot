a3faec14c6520da22e32ae5049073ee2f66269d3
GslGitHub <gsljava@gmail.com>
2020-12-23 21:15:55 +0800

    gsl_springboot

---
+++ springboot/gslspringboot/src/main/java/com/gslspringboot/controller/HelloController.java
@@ -0,0 +1,1 @@
+package com.gslspringboot.controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RestController;@RestControllerpublic class HelloController {    @GetMapping("hello")    public String hello(){        return "hello spring";    }}
